0364e11d0b672bf77ae2de307e186c7bb89b0c8a
NNdila <nicole.wambua@strathmore.edu>
2024-07-11 23:16:14 +0300

    fixed signup

--- signup.php
+++ signup.php
@@ -1,65 +1,23 @@
+<?php include 'header.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="reg.css">
+    <title>Sign Up - heBrews Coffee Shop</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-    <div class="travel">
-        <section id="travel" class="header-image">
-        </section>
-        </div>
-    <div class="center">
-        <h1>Sign Up</h1>
-        <div class="line"></div>   
-        <form method="post">
-            <div class="txt_field">
-                <input type="text"  name="people" required />
-
-                <span></span>
-                <label>Full Name</label>
-              </div>
-          <div class="txt_field">
-            <input type="text" name="username" required />
-            <span></span>
-            <label>Username</label>
-          </div>
-          <div class="txt_field">
-            <input type="password" name="password" required />
-            <span></span>
-            <label>Password</label>
-          </div>
-          <input type="submit" value="Sign Up" />
-          <br>
+    <div class="main-content">
+        <h2>Create Your Account</h2>
+        <form method="post" action="connection.php">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name" required><br><br>
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" name="signup" value="Sign Up">
         </form>
-      </div>
-      <br>
-      <footer>
-        <p> 2024 Travel Guide. All rights reserved.</p>
-    </footer>
-
-    <?php
-include 'connection.php';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['action'] == "create") {
-    $people = $_POST['people'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-
-    $stmt = $conn->prepare("INSERT INTO signup (people, username, keypassword)
-    VALUES (?, ?, ?)");
-    $stmt->bind_param("isss", $people, $username, $password);
-    $stmt->execute();
-    echo "Registration successful!";
-    $stmt->close();
-
-    $conn->close();
-}
-?>
-
+    </div>
+<?php include 'footer.php'; ?>
 </body>
 </html>
